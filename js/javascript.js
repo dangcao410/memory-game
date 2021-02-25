7faeee5d0378984e55a96f6008db74c4c9d68767
dangcao <dangcao410@gmail.com>
2021-02-25 14:04:38 +0700

    Update images.

---
+++ js/javascript.js
@@ -0,0 +1,51 @@
+// Card data
+const cardsArray = [
+    {
+        name: 'chelsea',
+        img: 'img/chelsea.png',
+    },
+    {
+        name: 'mancity',
+        img: 'img/mancity.png',
+    },
+    {
+        name: 'psg',
+        img: 'img/psg.png',
+    },
+    {
+        name: 'inter',
+        img: 'img/inter.png',
+    },
+    {
+        name: 'real',
+        img: 'img/real.png',
+    },
+    {
+        name: 'barca',
+        img: 'img/barca.png',
+    },
+    {
+        name: 'bayern',
+        img: 'img/bayern.png',
+    },
+    {
+        name: 'dortmund',
+        img: 'img/dortmund.png',
+    },
+    {
+        name: 'porto',
+        img: 'img/porto.png',
+    },
+    {
+        name: 'marseille',
+        img: 'img/marseille.png',
+    },
+    {
+        name: 'olympiacos',
+        img: 'img/olympiacos.png',
+    },
+    {
+        name: 'zenit',
+        img: 'img/zenit.png',
+    },
+]
